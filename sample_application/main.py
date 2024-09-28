706b1582d7985964dd9085a735fc18152ccae1a0
abhi2596 <abhijeetchintakunta@gmail.com>
2024-09-28 00:56:12 -0500

    "changed file logic"

--- sample_application/main.py
+++ sample_application/main.py
@@ -6,14 +6,13 @@ st.title("Tax Declaration Form")
 uploaded_files = st.file_uploader("Upload a Tax Declaration Form",accept_multiple_files=True)
 
 if len(uploaded_files) != 0:
+    os.mkdir("tax_declaration_files")
     for file in uploaded_files:
-        with open(f"files/{file.name}","wb") as f:
+        with open(f"tax_declaration_files/{file.name}","wb") as f:
             f.write(file.read())
     response = tax_declaration_summary("files")
     if response is not None:
         st.markdown(response)
-    for filename in os.listdir("files"):
-        file_path = os.path.join("files", filename)
-        os.remove(file_path)
+    shutil.rmtree("tax_declaration_files")
     
     
